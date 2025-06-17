a36064af00d17b737a5c3c9fc3ce4b6843dd88e0
Sayali2131-tech <Sayali21041996@gmail.com>
2025-06-17 17:42:06 +0530

    edited

--- src/test/java/com/TestRunner/runner.java
+++ src/test/java/com/TestRunner/runner.java
@@ -1,14 +1,15 @@
 package com.TestRunner;
 
-import org.junit.runner.RunWith;
 
-import io.cucumber.junit.Cucumber;
-import io.cucumber.junit.CucumberOptions;
+import io.cucumber.testng.CucumberOptions;
+import io.cucumber.testng.AbstractTestNGCucumberTests;
 
-@RunWith(Cucumber.class)
+//@RunWith(Cucumber.class)
 @CucumberOptions(features="src/test/resources/features",glue= {"com/StepDefinitions"},
-                    plugin= {"com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:"},tags = "@tc002")
+                    plugin= {
+                    		
+           "com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:"},tags = "@tc002")
 
-public class runner {
+public class runner extends AbstractTestNGCucumberTests{
 
 }
